06a21430dd8e8b70f36d5a2769c7c54e10e2db5d
AustinKurtis <austin.jay.kurtis@gmail.com>
2018-02-02 14:43:11 -0600

    Modify list contents

--- CollectionsCS/Program.cs
+++ CollectionsCS/Program.cs
@@ -9,10 +9,17 @@ namespace CollectionsCS
         static void Main(string[] args)
         {
             var names = new List<string> { "Austin", "Ana", "Felipe" };
+            Console.WriteLine();
+            names.Add("Maria");
+            names.Add("Bill");
+            names.Remove("Ana");
             foreach (var name in names)
             {
                 Console.WriteLine($"Hello {name.ToUpper()}!");
             }
+            Console.WriteLine($"My name is {names[0]}.");
+            Console.WriteLine($"I've added {names[2]} and {names[3]} to the list.");
+            Console.WriteLine($"The list as {names.Count} people on it.");
         }
     }
 }
