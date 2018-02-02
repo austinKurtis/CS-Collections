0a0444b87a3075bd659b0537ddaad06303498c70
AustinKurtis <austin.jay.kurtis@gmail.com>
2018-02-02 15:15:05 -0600

    Search and sort lists

--- CollectionsCS/Program.cs
+++ CollectionsCS/Program.cs
@@ -20,6 +20,17 @@ namespace CollectionsCS
             Console.WriteLine($"My name is {names[0]}.");
             Console.WriteLine($"I've added {names[2]} and {names[3]} to the list.");
             Console.WriteLine($"The list as {names.Count} people on it.");
+            var index = names.IndexOf("Felipe");
+            if (index != -1)
+                Console.WriteLine($"The name {names[index]} is at index {index}.");
+            var notFound = names.IndexOf("Not Found");
+            Console.WriteLine($"When an item is not found IndexOf returns {notFound}");
+
+            names.Sort();
+            foreach (var name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
         }
     }
 }
