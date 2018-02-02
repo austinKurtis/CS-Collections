d8f26c6e119d67716fefcf97a9ec663f3c0c6a9c
AustinKurtis <austin.jay.kurtis@gmail.com>
2018-02-02 15:19:22 -0600

    Lists of other types

--- CollectionsCS/Program.cs
+++ CollectionsCS/Program.cs
@@ -31,6 +31,15 @@ namespace CollectionsCS
             {
                 Console.WriteLine($"Hello {name.ToUpper()}!");
             }
+
+            var fibonacciNumbers = new List<int> { 1, 1 };
+            var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+            var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+            fibonacciNumbers.Add(previous + previous2);
+
+            foreach (var item in fibonacciNumbers)
+                Console.WriteLine(item);
         }
     }
 }
